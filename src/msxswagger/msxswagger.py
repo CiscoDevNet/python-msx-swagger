41ef3b9f010656b64e9a2cd43a1e3fae373685f4
hagraham <79648947+hagraham@users.noreply.github.com>
2021-05-27 15:11:10 -0400

    US35110 - MSX SSO Integration (#1)
    * Updates to support MSX SSO integration.
    
    * Removed unused Resource module.
    
    * Added screenshot showing a secure Swagger page.
    
    * Fixed typographic errors in README.

--- src/msxswagger/msxswagger.py
+++ src/msxswagger/msxswagger.py
@@ -2,15 +2,17 @@
 # Copyright (c) 2021 Cisco Systems, Inc and its affiliates
 # All rights reserved
 #
-from flask_restplus import Resource, Api
-from flask import Blueprint, render_template, Response, json
+from flask_restplus import Api
+from flask import Blueprint, render_template, json, jsonify, Flask
 
 
 class Sso:
-    def __init__(self, base_url='http://localhost:9103/idm',
-                 token_path='/v2/token',
-                 authorize_path='/v2/authorize',
-                 client_id='', client_secret=''):
+    def __init__(self,
+                 base_url="http://localhost:9103/idm",
+                 token_path="/v2/token",
+                 authorize_path="/v2/authorize",
+                 client_id="",
+                 client_secret=""):
         self.base_url = base_url
         self.token_path = self.base_url + token_path
         self.authorize_path = self.base_url + authorize_path
@@ -25,84 +27,104 @@ class Security:
 
 
 class DocumentationConfig:
-    def __init__(self, root_path: str, swagger_json_path: str,
-                 swagger_ui: str, spec_version, security: Security):
+    def __init__(self,
+                 root_path: str,
+                 ui_path="/swagger",
+                 resource_path="/swagger-resources",
+                 api_path="/apidocs.json",
+                 spec_version="3.0.0",
+                 security=Security(False)):
+
         self.root_path = root_path
-        self.swagger_json_path = swagger_json_path  # default= swagger json
-        self.swagger_ui = swagger_ui  # i.e. /swaggerui
+        self.ui_path = ui_path
+        self.resource_path = resource_path
+        self.api_path = api_path
         self.spec_version = spec_version
-        self.security = security  # security class
-
-
-class AppInfo:
-    def __init__(self, name: str, description: str, version):
-        self.name = name
-        self.description = description
-        self.version = version
-
-
-class MSXSwaggerDefaultConfig:
-    def __init__(self, app, root_path='/sampleservice'):
-        self.documentation_config = DocumentationConfig(root_path, root_path + '/swagger.json',
-                                                        root_path + '/swaggerui', '3.0.0', Security(False))
-        self.app_info = AppInfo(name='sample', description='This is a sample service', version=1.0)
-
-        self.api = Api(
-            Blueprint('swagger', __name__, url_prefix=self.documentation_config.root_path,
-                      template_folder='templates',
-                      static_folder='static', static_url_path='/%s' % __name__), doc=False)
-        self.resource = Resource
-
-        @app.route(self.documentation_config.swagger_json_path)
-        def swagger_json():
-            content = str(json.dumps(self.api.__schema__))
-            return Response(content,
-                            mimetype='application/json',
-                            headers={'Content-Disposition': 'attachment;filename=swagger.json'})
-
-        @app.route(self.documentation_config.swagger_ui)  # url for swagger ui
-        def swagger_ui():
-            return render_template('index.html', swagger_json=self.documentation_config.swagger_json_path), 200
+        self.security = security
 
 
 class MSXSwaggerConfig:
-    def __init__(self, app, app_info=AppInfo('sample', 'This is a sample service', 1.0),
-                 documentation_config=DocumentationConfig('', '/swagger.json', '/swaggerui', '3.0.0', Security(False)),
-                 disable_swagger_json_generation=False):
-        self.documentation_config = documentation_config
-        self.app_info = app_info
-        self.resource = Resource
+    def __init__(self,
+                 app: Flask,
+                 documentation_config: DocumentationConfig,
+                 swagger_resource=None):
 
-        if disable_swagger_json_generation is False:
-            @app.route(self.documentation_config.swagger_json_path)
-            def swagger_json():
-                content = str(json.dumps(self.api.__schema__))
-                return Response(content,
-                                mimetype='application/json',
-                                headers={'Content-Disposition': 'attachment;filename=swagger.json'})
+        blueprint = Blueprint(
+            name='swagger',
+            import_name=__name__,
+            url_prefix=documentation_config.root_path,
+            template_folder='templates',
+            static_folder='static')
+        self.api = Api(blueprint)
 
-        if documentation_config.security.enabled:
-            auth = {
-                'oauth2': {
-                    'type': 'oauth2',
-                    'authorizationUrl': self.documentation_config.security.sso.authorize_path,
-                    'tokenUrl': self.documentation_config.security.sso.token_path,
-                    'flow': 'password',
-                    'scopes': {
-                        'read': 'read',
-                    }
-                }
-            }
-            self.api = Api(
-                Blueprint('swagger', __name__, url_prefix=self.documentation_config.root_path,
-                          template_folder='templates',
-                          static_folder='static', static_url_path='/%s' % __name__), security='oauth2', authorizations=auth)
-        else:
-            self.api = Api(
-                Blueprint('swagger', __name__, url_prefix=self.documentation_config.root_path,
-                          template_folder='templates',
-                          static_folder='static', static_url_path='/%s' % __name__), doc=False)
-
-        @app.route(self.documentation_config.swagger_ui)  # url for swagger ui
+        @blueprint.route(documentation_config.ui_path)
         def swagger_ui():
-            return render_template('index.html', swagger_json=self.documentation_config.swagger_json_path), 200
+            config = documentation_config
+            template = "secure.html" if config.security.enabled else "insecure.html",
+            swagger_json = config.root_path + config.resource_path + config.api_path
+            return render_template(template, swagger_json=swagger_json), 200
+
+        @blueprint.route(documentation_config.resource_path + documentation_config.api_path)
+        def swagger_resource_json():
+            if swagger_resource:
+                with app.open_resource(swagger_resource, "r") as file:
+                    contents = file.read()
+            else:
+                contents = str(json.dumps(self.api.__schema__))
+            return contents, 200
+
+        @blueprint.route("/swagger-sso-redirect.html")
+        def swagger_sso_redirect_html():
+            return render_template('swagger-sso-redirect.html'), 200
+
+        @blueprint.route(documentation_config.resource_path)
+        def swagger_resources_configuration():
+            return jsonify([{
+                "name": "platform",
+                "location": documentation_config.resource_path + documentation_config.api_path,
+                "url": documentation_config.resource_path + documentation_config.api_path,
+                "swaggerVersion": documentation_config.spec_version,
+            }])
+
+        @blueprint.route(documentation_config.resource_path + "/configuration/ui")
+        def swagger_resources_configuration_ui():
+            return jsonify({
+                       "deepLinking": True,
+                       "displayOperationId": False,
+                       "defaultModelsExpandDepth": 1,
+                       "defaultModelExpandDepth": 1,
+                       "defaultModelRendering": "example",
+                       "displayRequestDuration": False,
+                       "docExpansion": "none",
+                       "filter": False,
+                       "operationsSorter": "alpha",
+                       "showExtensions": False,
+                       "showCommonExtensions": False,
+                       "tagsSorter": "alpha",
+                       "validatorUrl": "",
+                       "supportedSubmitMethods": [
+                           "get",
+                           "put",
+                           "post",
+                           "delete",
+                           "options",
+                           "head",
+                           "patch",
+                           "trace"
+                       ],
+                       "swaggerBaseUiUrl": ""
+                   })
+
+        @blueprint.route(documentation_config.resource_path + "/configuration/security")
+        def swagger_resources_configuration_security():
+            return jsonify({})
+
+        @blueprint.route(documentation_config.resource_path + "/configuration/security/sso")
+        def swagger_resources_configuration_security_sso():
+            sso = documentation_config.security.sso
+            return jsonify({
+                "authorizeUrl": sso.authorize_path,
+                "clientId": sso.client_id,
+                "tokenUrl": sso.token_path,
+                "clientSecret": sso.client_secret if sso.client_secret else "",
+            })
