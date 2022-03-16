8e9817100dd22de2b9251df44aa6bceffbfd410e
Haemish Graham <hagraham@cisco.com>
2022-03-16 14:21:06 -0400

    Updated dependencies to latest versions.

--- setup.py
+++ setup.py
@@ -15,18 +15,18 @@ def current_path(file_name):
 
 setup(
     name='msxswagger',
-    version='0.6.0',
+    version='0.7.0',
     author="Cisco MSX",
     author_email="ananasta@cisco.com",
     description='A package that enables swagger ui in your python web application',
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(),
-    python_requires=">=3.0",
+    python_requires=">=3.6",
     install_requires=[
-        "Flask==1.1.2",
+        "Flask==2.0.3",
         "Flask-Cors==3.0.10",
-        "flask-restplus==0.13.0",
-        "Werkzeug==0.16.1",
+        "flask-restx==0.5.1",
+        "Werkzeug==2.0.3",
     ],
 )
